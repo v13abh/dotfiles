0fd321ac21e5728d645ac5506393390f7d10f56e
Chris Cone <chris.cone@ou.edu>
2016-08-30 11:59:16 -0500

    Added digrc

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc gitconfig liquidprompt"    # list of files/folders to symlink in homedir
+files="bashrc vimrc gitconfig liquidprompt digrc"    # list of files/folders to symlink in homedir
 
 ##########
 
